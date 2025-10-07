e2e9c0a6fd23328b3217010db4e2eff752988fe2
Afonso Carvalho <afonsomiguelcarvalho2006@gmail.com>
2025-10-07 15:16:21 +0100

    Configuring Frontend

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import '../styles/HomeStyle.css'
+
+function Homepage () {
+    return(
+        <div>
+            <header>
+                <div className="headerDiv">
+                <div className="leftHeader"><p>☐☐☐ INSIGHTXR (name in logo image)</p></div>
+                <div className="rightHeader"><p>NAVIGATION, </p></div></div>
+            </header>
+        </div>
+    )
+}
+
+export default Homepage;
